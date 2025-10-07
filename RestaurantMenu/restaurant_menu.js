afd56af281b63282ef1d83c575f8b111c8dc5ff5
Ehtisham-Rana <167960822+Ehtisham-Rana@users.noreply.github.com>
2025-10-07 07:12:44 -0400

    added employee details

--- RestaurantMenu/restaurant_menu.js
+++ RestaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $18', 'Pasta - $15.50', 'Burger - $10', 'Salmon - $18.80'];
+const dessertMenu = ['Cake - $21.50', 'Ice Cream - $7.75', 'Pudding - $25', 'Fruit Salad - $9.99'];
 //map() array method
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
