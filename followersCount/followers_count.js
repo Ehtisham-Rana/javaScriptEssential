07e7c0ab7bbf2cf55a77b0d0a1b3b25856e98ec9
Ehtisham-Rana <167960822+Ehtisham-Rana@users.noreply.github.com>
2025-10-07 06:03:45 -0400

    created follower counter

---
+++ followersCount/followers_count.js
@@ -0,0 +1,24 @@
+let count = 0;
+
+function increaseCount() {
+  count++; 
+  displayCount();
+  checkCountValue();
+}
+
+function checkCountValue() {
+    if (count === 10) {
+      alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+      alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function displayCount() {
+document.getElementById('countDisplay').innerHTML=count;
+}
+
+function resetCount() {
+    document.getElementById('countDisplay').innerHTML= 0 ;
+    alert( "Followers count has been reset")
+}
